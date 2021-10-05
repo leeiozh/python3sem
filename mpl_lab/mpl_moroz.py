c6a1b676ee4b2bc963fecfc0b5fa9abb4588b1c1
ezhova.ea <ezhova.ea@phystech.edu>
2021-10-05 19:13:26 +0300

    init commit

---
+++ mpl_lab/mpl_moroz.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+number_of_datas = 5
+
+fig, axs = plt.subplots(nrows=1, ncols=number_of_datas)
+
+x = [[] for i in range(number_of_datas)]
+y = [[] for i in range(number_of_datas)]
+
+for i in range(number_of_datas):
+    with open("data/00" + str(i + 1) + ".dat", 'r') as file:
+        for line in file:
+            try:
+                y[i].append(float(line.split()[1]))
+                x[i].append(float(line.split()[0]))
+            except:
+                pass
+    axs[i].scatter(x[i], y[i], s=3)
+
+figure = plt.gcf()
+figure.set_size_inches(18, 6)
+plt.savefig("dead.png")

1696fe423b9ed5200c7fbc21fa1cef1fbc92c5d2
ezhova.ea <ezhova.ea@phystech.edu>
2021-11-13 15:33:46 +0300

    add signal

---
+++ numpy0911/signal_filter.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, axs = plt.subplots(nrows=1, ncols=2)
+
+input_data = np.fromfile("signal03.dat", dtype=float)
+output_data = np.empty(len(input_data))
+
+for i in range(len(output_data)):
+    if i > 8:
+        output_data[i] = np.mean(input_data[i - 9: i + 1])
+    else:
+        output_data[i] = np.mean(input_data[0: i + 1])
+
+axs[0].plot(np.arange(0, len(input_data)), input_data)
+axs[0].set_title("Сырой сигнал")
+axs[1].plot(np.arange(0, len(output_data)), output_data)
+axs[1].set_title("Фильтрованный сигнал")
+
+plt.grid()
+figure = plt.gcf()
+figure.set_size_inches(18, 6)
+plt.savefig("signal03_filter.png")

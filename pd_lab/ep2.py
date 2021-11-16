3e8f65bd2be1be9f75d9245a51c10716faab0c0c
ezhova.ea <ezhova.ea@phystech.edu>
2021-11-16 23:43:34 +0300

    fix ep3 in pd

--- pd_lab/ep2.py
+++ pd_lab/ep2.py
@@ -9,15 +9,15 @@ df_cargo = df.groupby('CARGO')
 
 ax_0 = df_cargo["PRICE"].sum().plot(kind='barh', color=colors)
 ax_0.set_title("sums")
-plt.savefig("flights_sum.png")
+plt.savefig("ep2_sum.png")
 plt.clf()
 
 ax_1 = df_cargo["WEIGHT"].sum().plot(kind='barh', color=colors)
 ax_1.set_title("weights")
-plt.savefig("flights_weight.png")
+plt.savefig("ep2_weight.png")
 plt.clf()
 
 ax_2 = df_cargo["WEIGHT"].count().plot(kind='barh', color=colors)
 ax_2.set_title("flights")
-plt.savefig("flights_flights.png")
+plt.savefig("ep2_flights.png")
 plt.clf()
--- pd_lab/ep3.py
+++ pd_lab/ep3.py
@@ -1,29 +1,27 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+
+figure = plt.gcf()
+figure.set_size_inches(12, 6)
+colors = ["#FF0000", "#FF7700", "#FFAB00", "#FFD500", "#FFFF00", "#DEFF00", "#9AFF00", "#1AFF00"]
 
 ejudge = pd.read_html("results_ejudge.html")[0]
 groups = pd.read_excel("students_info.xlsx")
 
 data = pd.merge(groups, ejudge, on='User')
 
-group_faculty = groups['group_faculty'].unique()
-group_out = np.sort(groups['group_out'].unique())
+res_fac = data.groupby('group_faculty')['Solved'].mean()
+res_out = data.groupby('group_out')['Solved'].mean()
 
-res_fac = [data[data['group_faculty'] == i]['Solved'].mean() for i in group_faculty]
-res_out = [data[data['group_out'] == i]['Solved'].mean() for i in group_out]
+ax_0 = res_fac.plot(kind='bar', color=colors)
+ax_0.set_title("group_in")
+plt.savefig("ep3_group_in.png")
+plt.clf()
 
-fig, axs = plt.subplots(nrows=1, ncols=2)
-colors = ["#FF0000", "#FF7700", "#FFAB00", "#FFD500", "#FFFF00", "#DEFF00", "#9AFF00", "#1AFF00"]
-
-axs[0].bar([str(i) for i in group_faculty], res_fac, color=colors)
-axs[0].set_title("group_in")
-axs[1].bar([str(i) for i in group_out], res_out, color=colors)
-axs[1].set_title("group_out")
-
-figure = plt.gcf()
-figure.set_size_inches(12, 6)
-plt.savefig("number_of_solved_tasks.png")
+ax_1 = res_out.plot(kind='bar', color=colors)
+ax_1.set_title("group_in")
+plt.savefig("ep3_group_out.png")
+plt.clf()
 
 print("Groups_in with best students....")
 for i in data[(data['H'] > 0) | (data['G'] > 0)]['group_faculty']:

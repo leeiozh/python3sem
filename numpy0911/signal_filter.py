948b9cce95b4da1f179b49432e29f97579e5f06e
ezhova.ea <ezhova.ea@phystech.edu>
2021-11-29 00:29:59 +0300

    fix calc mean

--- numpy0911/signal_filter.py
+++ numpy0911/signal_filter.py
@@ -7,7 +7,7 @@ input_data = np.loadtxt("signal03.dat")
 output_data = np.empty(len(input_data))
 
 output_data[9:] = (input_data[:-9] + input_data[1:-8] + input_data[2:-7] + input_data[3:-6] + input_data[4:-5] +
-                   input_data[5:-4] + input_data[6:-3] + input_data[7:-2] + input_data[8:-1] + input_data[9:]) / 9
+                   input_data[5:-4] + input_data[6:-3] + input_data[7:-2] + input_data[8:-1] + input_data[9:]) / 10
 output_data[:9] = [np.mean(input_data[0: i + 1]) for i in range(9)]
 
 axs[0].plot(np.arange(0, len(input_data)), input_data)
